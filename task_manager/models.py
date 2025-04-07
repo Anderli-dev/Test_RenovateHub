c4da94502230f004050b64ea162eabf02f56b1b4
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-07 10:18:37 +0300

    added project and task models. +migration for this models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        LOW = 1, _("Low")
+        MEDIUM = 2, _("Medium")
+        HIGH = 3, _("High")
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    name = models.CharField(max_length=255)
+    status = models.BooleanField(default=False)
+    priority = models.PositiveSmallIntegerField(
+        choices=Priority.choices,
+        default=Priority.MEDIUM,
+    )
+
+    class Meta:
+        ordering = ["status", "priority"]
+        
+    def __str__(self):
+        return self.name
+    
