100d50235431cd3889815cbac369ae128f0eafa3
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-09 11:15:19 +0300

    changed the task creation for the updated model

--- task_manager/models.py
+++ task_manager/models.py
@@ -30,7 +30,7 @@ class Task(models.Model):
     order = models.PositiveIntegerField(default=0, db_index=True)
 
     class Meta:
-        ordering = ["-deadline", "-status", "-priority"]
+        ordering = ["order", "-deadline", "-status", "-priority"]
         
     def __str__(self):
         return self.name
--- task_manager/views/task.py
+++ task_manager/views/task.py
@@ -5,13 +5,15 @@ from task_manager.forms import TaskCreateForm
 from task_manager.models import Project, Task
 from django.template.response import TemplateResponse
 from django.views.decorators.http import require_POST
-
+from django.db.models import Max
 @require_POST
 def task_add(request: HttpRequest):
     form = TaskCreateForm(request.POST)
     project = get_object_or_404(Project, id=request.POST.get("project_id"))
     if form.is_valid():
         task = form.save(commit=False)
+        max_order = Task.objects.filter(project=project).aggregate(Max("order"))["order__max"]
+        task.order = (max_order + 1) if max_order is not None else 0
         task.project = project
         task.save()
     else: 
