cbe312a513cef8c34be13b9fc3f2097254323a06
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-10 09:52:15 +0300

    added auth check. also login, ang logput pages styling

--- task_manager/views/home.py
+++ task_manager/views/home.py
@@ -6,5 +6,8 @@ from django.template.response import TemplateResponse
 
 @for_htmx(use_block_from_params=True)
 def home(request: HttpRequest):
-    projects = Project.objects.prefetch_related('tasks').all().order_by('id')
+    if request.user.is_authenticated:
+        projects = Project.objects.filter(user=request.user).prefetch_related('tasks').all().order_by('id')
+    else:
+        projects = []
     return TemplateResponse(request, 'home.html', {'projects': projects})
