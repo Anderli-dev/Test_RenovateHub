b7ab1c18102d893f3e19eecaf28e13064d67e664
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-11 14:48:45 +0300

    created separate folder for utils

---
+++ task_manager/utils/render_htmx_error.py
@@ -0,0 +1,13 @@
+import json
+
+from django.http import HttpRequest
+from django.http.request import HttpRequest
+from django.template.response import TemplateResponse
+
+def render_htmx_error(request: HttpRequest, message: str, status_code: int, trigger: str = "error"):
+    context = {"error_message": message}
+    response = TemplateResponse(request, "error_modal.html", context=context, status=status_code)
+    response["Hx-Trigger"] = json.dumps({
+        trigger: message,
+    })
+    return response
