014375fe07136112d1289eae21b508e9b5625fc6
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-11 08:28:53 +0300

    added test for form

--- task_manager/tests/test_forms.py
+++ task_manager/tests/test_forms.py
@@ -1,5 +1,7 @@
+from django.urls import reverse
 import pytest
 from task_manager.forms import ProjectForm, TaskCreateForm, TaskEditForm
+from task_manager.models import Project
 
 @pytest.mark.parametrize("name, valid", [
     ("Valid name", True),
@@ -23,3 +25,15 @@ def test_task_edit_form_clean_name():
     form = TaskEditForm(data={"name": "<bad>", "priority": 2, "deadline": "2025-04-11"})
     assert not form.is_valid()
     assert "HTML-теги заборонено." in str(form.errors)
+
+@pytest.mark.django_db
+def test_task_add_invalid_form(client, user):
+    client.force_login(user)
+    project = Project.objects.create(user=user, name="Proj")
+    response = client.post(
+        reverse("task_add"),
+        data={"name": "<invalid>", "project_id": project.id},
+        HTTP_HX_REQUEST="true"
+    )
+    assert response.status_code == 403
+    assert b"Wrong input data" in response.content
