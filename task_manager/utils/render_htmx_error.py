e7f1e796c1a6e6e186e18a146757890a20169324
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-11 22:34:54 +0300

    moved error modal to modalst folder. fixed error-observer script

--- task_manager/utils/render_htmx_error.py
+++ task_manager/utils/render_htmx_error.py
@@ -6,7 +6,7 @@ from django.template.response import TemplateResponse
 
 def render_htmx_error(request: HttpRequest, message: str, status_code: int, trigger: str = "error"):
     context = {"error_message": message}
-    response = TemplateResponse(request, "error_modal.html", context=context, status=status_code)
+    response = TemplateResponse(request, "elements/modals/error_modal.html", context=context, status=status_code)
     response["Hx-Trigger"] = json.dumps({
         trigger: message,
     })
