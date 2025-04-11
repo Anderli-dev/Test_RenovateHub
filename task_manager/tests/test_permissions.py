f44e621270d5d35793c71e28d4cf2536729584f2
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-11 08:14:03 +0300

    added tests for permissions

---
+++ task_manager/tests/test_permissions.py
@@ -0,0 +1,7 @@
+import pytest
+from django.urls import reverse
+
+@pytest.mark.django_db
+def test_create_project_denied_for_anonymous(client):
+    response = client.get(reverse("modals_add_project"))
+    assert response.status_code == 302  # redirect to login
